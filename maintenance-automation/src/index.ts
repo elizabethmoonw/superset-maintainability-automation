0c77b39f79fb13615f63a912ea2d43b6fb20b9aa
Elizabeth Moon <69644849+elizabethmoonw@users.noreply.github.com>
2026-08-03 07:44:35 -0700

    feat(maintenance-automation): implement Checkpoint 4 - GitHub approval workflow

--- maintenance-automation/src/index.ts
+++ maintenance-automation/src/index.ts
@@ -1,2 +1,26 @@
 export { isAcceptedFinding, Finding } from "./isAcceptedFinding";
-export { parseKnipReport, KnipReport, NormalizedFinding } from "./parseKnipReport";
+export {
+  parseKnipReport,
+  KnipReport,
+  NormalizedFinding,
+} from "./parseKnipReport";
+export {
+  EVIDENCE_NOTICE,
+  MAX_ACTIONABLE_FINDINGS,
+  calculateDigest,
+  calculateReportDigest,
+  createFindingKey,
+  generateActionableBatch,
+  generateRunArtifacts,
+  serializeActionableBatch,
+} from "./generateActionableBatch";
+export type {
+  ActionableBatch,
+  ActionableBatchFinding,
+  GeneratedRunArtifacts,
+  MaintenanceRunState,
+  RunContext,
+  RunStatus,
+  ScanCount,
+  ScanCounts,
+} from "./generateActionableBatch";
